ff55a2bd1632fa1377880d1678a5d2f0d2ed1964
Adrien490 <adrien.poirier49@gmail.com>
2024-12-27 15:37:54 +0100

    Update login-provider.ts

--- api/auth/login-provider.ts
+++ api/auth/login-provider.ts
@@ -1,28 +1,12 @@
 "use server";
 
 import { signIn } from "@/auth";
-import { z } from "zod";
-
-const LoginProviderSchema = z.object({
-	provider: z.enum(["google", "github"]),
-});
 
 export default async function loginProvider({
 	provider,
 }: {
 	provider: string;
 }) {
-	const validatedFields = LoginProviderSchema.safeParse({
-		provider,
-	});
-
-	if (!validatedFields.success) {
-		return {
-			success: false,
-			message: "Invalid provider",
-		};
-	}
-
 	await signIn(provider, {
 		redirectTo: "/dashboard",
 	});
