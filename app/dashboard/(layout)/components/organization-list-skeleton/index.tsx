aeca6b22988ee58575a611d2af50eb91478bfbd1
Adrien490 <adrien.poirier49@gmail.com>
2025-03-24 14:02:04 +0100

    Update index.tsx

--- app/dashboard/(layout)/components/organization-list-skeleton/index.tsx
+++ app/dashboard/(layout)/components/organization-list-skeleton/index.tsx
@@ -2,7 +2,6 @@
 
 import { OrganizationCardSkeleton } from "@/app/dashboard/(layout)/components/organization-card-skeleton";
 import { ViewType } from "@/shared/components/toggle-view/types";
-import { Skeleton } from "@/shared/components/ui/skeleton";
 
 type OrganizationListSkeletonProps = {
 	viewType?: ViewType;
@@ -13,15 +12,6 @@ export default function OrganizationListSkeleton({
 }: OrganizationListSkeletonProps) {
 	return (
 		<div className="space-y-6">
-			{/* En-tête avec zone de recherche et boutons */}
-			<div className="flex justify-between items-center">
-				<Skeleton className="h-9 w-[300px]" />
-				<div className="flex gap-2">
-					<Skeleton className="h-9 w-[120px]" />
-					<Skeleton className="h-9 w-[170px]" />
-				</div>
-			</div>
-
 			{/* Grille d'organisations */}
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
 				{Array.from({ length: 6 }).map((_, i) => (
