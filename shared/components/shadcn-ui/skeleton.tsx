8a579e3601faa5fc5812b8483b8784d74ddc2fce
Adrien490 <adrien.poirier49@gmail.com>
2025-04-18 10:58:17 +0200

    Update skeleton.tsx

--- shared/components/shadcn-ui/skeleton.tsx
+++ shared/components/shadcn-ui/skeleton.tsx
@@ -1,10 +1,15 @@
 import { cn } from "@/shared/utils";
+import * as React from "react";
 
 function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
 	return (
 		<div
 			data-slot="skeleton"
-			className={cn("bg-card animate-pulse rounded-md", className)}
+			aria-hidden="true"
+			className={cn(
+				"bg-muted/70 relative overflow-hidden rounded-md animate-pulse before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/10 before:to-transparent",
+				className
+			)}
 			{...props}
 		/>
 	);
