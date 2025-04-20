52ac4a59abb5da35e6e6b806398f813f3cedc315
Adrien490 <adrien.poirier49@gmail.com>
2025-04-20 18:11:53 +0200

    test

--- domains/client/features/delete-multiple-clients/hooks/use-delete-multiple-clients.ts
+++ domains/client/features/delete-multiple-clients/hooks/use-delete-multiple-clients.ts
@@ -1,16 +1,24 @@
 "use client";
 
+import { useSelectionContext } from "@/shared/contexts";
 import { createToastCallbacks, withCallbacks } from "@/shared/utils";
 import { useActionState } from "react";
+import { toast } from "sonner";
 import { deleteMultipleClients } from "../actions";
 import { deleteMultipleClientsSchema } from "../schemas";
 
 export const useDeleteMultipleClients = () => {
+	const { clearSelection } = useSelectionContext();
+
 	const [state, dispatch, isPending] = useActionState(
 		withCallbacks(
 			deleteMultipleClients,
 			createToastCallbacks<null, typeof deleteMultipleClientsSchema>({
 				loadingMessage: "Suppression des clients en cours...",
+				onSuccess: (data) => {
+					clearSelection();
+					toast.success(data?.message);
+				},
 			})
 		),
 		null
--- domains/supplier/features/delete-multiple-suppliers/hooks/use-delete-multiple-suppliers.ts
+++ domains/supplier/features/delete-multiple-suppliers/hooks/use-delete-multiple-suppliers.ts
@@ -1,16 +1,23 @@
 "use client";
 
+import { useSelectionContext } from "@/shared/contexts";
 import { createToastCallbacks, withCallbacks } from "@/shared/utils";
 import { useActionState } from "react";
+import { toast } from "sonner";
 import { deleteMultipleSuppliers } from "../actions";
 import { deleteMultipleSuppliersSchema } from "../schemas";
 
 export const useDeleteMultipleSuppliers = () => {
+	const { clearSelection } = useSelectionContext();
 	const [state, dispatch, isPending] = useActionState(
 		withCallbacks(
 			deleteMultipleSuppliers,
 			createToastCallbacks<null, typeof deleteMultipleSuppliersSchema>({
 				loadingMessage: "Suppression des fournisseurs en cours...",
+				onSuccess: (data) => {
+					clearSelection();
+					toast.success(data?.message);
+				},
 			})
 		),
 		null
