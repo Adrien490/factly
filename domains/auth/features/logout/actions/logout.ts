cb9d142ce9d39f6d570405745f1deb8262decf37
Adrien490 <adrien.poirier49@gmail.com>
2025-04-20 13:55:37 +0200

    Update logout.ts

--- domains/auth/features/logout/actions/logout.ts
+++ domains/auth/features/logout/actions/logout.ts
@@ -1,10 +1,22 @@
 "use server";
 
 import { auth } from "@/domains/auth/lib";
+import { ActionStatus, createErrorResponse } from "@/shared/types";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function logout() {
+	const session = await auth.api.getSession({
+		headers: await headers(),
+	});
+
+	if (!session) {
+		return createErrorResponse(
+			ActionStatus.UNAUTHORIZED,
+			"Vous n'êtes pas connecté"
+		);
+	}
+
 	await auth.api.signOut({ headers: await headers() });
 	redirect("/login");
 }
