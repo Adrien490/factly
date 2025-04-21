a64890bb44e0590abbd7cb9413a236e7e39bb1ae
Adrien490 <adrien.poirier49@gmail.com>
2025-04-21 15:55:55 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	experimental: {
 		authInterrupts: true,
-		dynamicIO: true,
+		useCache: true,
 		nodeMiddleware: true,
 		cacheLife: {
 			organizations: {
