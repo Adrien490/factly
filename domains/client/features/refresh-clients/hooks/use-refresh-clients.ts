e6a472678bcec52d2fc3f0a049f21ef6dd8e6a91
Adrien490 <adrien.poirier49@gmail.com>
2025-04-21 13:29:07 +0200

    test

--- domains/client/features/refresh-clients/hooks/use-refresh-clients.ts
+++ domains/client/features/refresh-clients/hooks/use-refresh-clients.ts
@@ -1,9 +1,19 @@
 "use client";
+import { createToastCallbacks, withCallbacks } from "@/shared/utils";
 import { useActionState } from "react";
 import { refreshClients } from "../actions";
+import { refreshClientsSchema } from "../schemas";
 
 export const useRefreshClients = () => {
-	const [state, dispatch, isPending] = useActionState(refreshClients, null);
+	const [state, dispatch, isPending] = useActionState(
+		withCallbacks(
+			refreshClients,
+			createToastCallbacks<null, typeof refreshClientsSchema>({
+				loadingMessage: "Rafraîchissement des clients en cours...",
+			})
+		),
+		null
+	);
 
 	return {
 		state,
--- domains/supplier/features/refresh-suppliers/hooks/use-refresh-suppliers.ts
+++ domains/supplier/features/refresh-suppliers/hooks/use-refresh-suppliers.ts
@@ -1,9 +1,19 @@
 "use client";
+import { createToastCallbacks, withCallbacks } from "@/shared/utils";
 import { useActionState } from "react";
 import { refreshSuppliers } from "../actions";
+import { refreshSuppliersSchema } from "../schemas";
 
 export const useRefreshSuppliers = () => {
-	const [state, dispatch, isPending] = useActionState(refreshSuppliers, null);
+	const [state, dispatch, isPending] = useActionState(
+		withCallbacks(
+			refreshSuppliers,
+			createToastCallbacks<null, typeof refreshSuppliersSchema>({
+				loadingMessage: "Rafraîchissement des fournisseurs en cours...",
+			})
+		),
+		null
+	);
 
 	return {
 		state,
