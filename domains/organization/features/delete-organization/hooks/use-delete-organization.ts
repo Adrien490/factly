ae7a0514655b91e8e60c6aee0ef59edcb9595fef
Adrien490 <adrien.poirier49@gmail.com>
2025-04-23 16:25:04 +0200

    Update use-delete-organization.ts

--- domains/organization/features/delete-organization/hooks/use-delete-organization.ts
+++ domains/organization/features/delete-organization/hooks/use-delete-organization.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import { useActionState } from "react";
-import { deleteOrganization } from "../actions";
+import { deleteOrganization } from "../actions/delete-organization";
 
 export function useDeleteOrganization() {
 	const [state, dispatch, isPending] = useActionState(
