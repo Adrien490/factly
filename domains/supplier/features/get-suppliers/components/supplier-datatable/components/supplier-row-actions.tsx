9cb9d4fbbfcdf98920f30111bf8d1687bacf6597
Adrien490 <adrien.poirier49@gmail.com>
2025-04-30 10:17:51 +0200

    Update supplier-row-actions.tsx

--- domains/supplier/features/get-suppliers/components/supplier-datatable/components/supplier-row-actions.tsx
+++ domains/supplier/features/get-suppliers/components/supplier-datatable/components/supplier-row-actions.tsx
@@ -1,4 +1,6 @@
 import { SUPPLIER_STATUSES } from "@/domains/supplier/constants";
+import { SUPPLIER_STATUS_TRANSITIONS } from "@/domains/supplier/constants/supplier-status-transitions";
+import { DeleteSupplierButton } from "@/domains/supplier/features/delete-supplier/components/delete-supplier-button";
 import { UpdateSupplierStatusButton } from "@/domains/supplier/features/update-supplier-status/components/update-supplier-status-button";
 import {
 	AlertDialog,
@@ -24,11 +26,13 @@ import {
 import { cn } from "@/shared/utils";
 import { SupplierStatus } from "@prisma/client";
 import {
+	Archive,
 	Edit2,
 	FileText,
 	MoreVerticalIcon,
 	Tag,
 	Trash,
+	Undo,
 	Users,
 } from "lucide-react";
 import Link from "next/link";
@@ -69,7 +73,6 @@ export function SupplierRowActions({ supplier }: SupplierRowActionsProps) {
 					>
 						<FileText className="h-4 w-4 mr-2" />
 						<span>Fiche fournisseur</span>
-						{/* Indicateur de chargement masqué par défaut */}
 						<LoadingIndicator className="ml-auto h-4 w-4 invisible" />
 					</Link>
 				</DropdownMenuItem>
@@ -91,7 +94,7 @@ export function SupplierRowActions({ supplier }: SupplierRowActionsProps) {
 								className={cn("flex w-full items-center")}
 							>
 								<Users className="h-4 w-4 mr-2" />
-								<span>Gestion des contacts</span>
+								<span>Gérer les contacts</span>
 							</Link>
 						</DropdownMenuItem>
 						<DropdownMenuSeparator />
@@ -104,7 +107,9 @@ export function SupplierRowActions({ supplier }: SupplierRowActionsProps) {
 								{SUPPLIER_STATUSES.filter(
 									(status) =>
 										status.value !== SupplierStatus.ARCHIVED &&
-										status.value !== supplier.status
+										SUPPLIER_STATUS_TRANSITIONS[supplier.status].includes(
+											status.value
+										)
 								).map((status) => (
 									<UpdateSupplierStatusButton
 										key={status.value}
@@ -131,7 +136,7 @@ export function SupplierRowActions({ supplier }: SupplierRowActionsProps) {
 									preventDefault
 									className="text-destructive focus:text-destructive"
 								>
-									<Trash className="text-destructive h-4 w-4 mr-2" />
+									<Archive className="text-destructive h-4 w-4 mr-2" />
 									<span>Archiver</span>
 								</DropdownMenuItem>
 							</AlertDialogTrigger>
@@ -162,33 +167,89 @@ export function SupplierRowActions({ supplier }: SupplierRowActionsProps) {
 				{isArchived && (
 					<>
 						<DropdownMenuSeparator />
+						<DropdownMenuSub>
+							<DropdownMenuSubTrigger>
+								<Undo className="h-4 w-4 mr-2" />
+								<span>Restaurer</span>
+							</DropdownMenuSubTrigger>
+							<DropdownMenuSubContent>
+								{SUPPLIER_STATUSES.filter(
+									(status) => status.value !== SupplierStatus.ARCHIVED
+								).map((status) => (
+									<AlertDialog key={status.value}>
+										<AlertDialogTrigger asChild>
+											<DropdownMenuItem preventDefault>
+												<div className="flex items-center gap-2">
+													<div
+														className="h-2 w-2 rounded-full"
+														style={{ backgroundColor: status.color }}
+													/>
+													<span>{status.label}</span>
+												</div>
+											</DropdownMenuItem>
+										</AlertDialogTrigger>
+										<AlertDialogContent>
+											<AlertDialogHeader>
+												<AlertDialogTitle>
+													Restaurer le fournisseur en{" "}
+													{status.label.toLowerCase()}
+												</AlertDialogTitle>
+												<AlertDialogDescription>
+													Cette action va restaurer le fournisseur
+													{supplier.name && (
+														<strong> {supplier.name}</strong>
+													)}{" "}
+													en statut {status.label.toLowerCase()}.
+													<br />
+													Cette action est réversible.
+												</AlertDialogDescription>
+											</AlertDialogHeader>
+											<AlertDialogFooter>
+												<AlertDialogCancel>Annuler</AlertDialogCancel>
+												<UpdateSupplierStatusButton
+													organizationId={supplier.organizationId}
+													id={supplier.id}
+													status={status.value}
+												>
+													<AlertDialogAction>Restaurer</AlertDialogAction>
+												</UpdateSupplierStatusButton>
+											</AlertDialogFooter>
+										</AlertDialogContent>
+									</AlertDialog>
+								))}
+							</DropdownMenuSubContent>
+						</DropdownMenuSub>
 						<AlertDialog>
 							<AlertDialogTrigger asChild>
-								<DropdownMenuItem preventDefault>
-									<Tag className="h-4 w-4 mr-2" />
-									<span>Remettre en actif</span>
+								<DropdownMenuItem
+									preventDefault
+									className="text-destructive focus:text-destructive"
+								>
+									<Trash className="text-destructive h-4 w-4 mr-2" />
+									<span>Supprimer définitivement</span>
 								</DropdownMenuItem>
 							</AlertDialogTrigger>
 							<AlertDialogContent>
 								<AlertDialogHeader>
-									<AlertDialogTitle>
-										Remettre le fournisseur en actif
+									<AlertDialogTitle className="text-destructive">
+										Êtes-vous sûr de vouloir supprimer définitivement ce
+										fournisseur ?
 									</AlertDialogTitle>
 									<AlertDialogDescription>
-										Cette action va remettre le fournisseur
-										{supplier.name && <strong> {supplier.name}</strong>} en
-										statut actif.
+										Cette action va supprimer définitivement le fournisseur
+										{supplier.name && <strong> {supplier.name}</strong>}.
+										<br />
+										Cette action est irréversible.
 									</AlertDialogDescription>
 								</AlertDialogHeader>
 								<AlertDialogFooter>
 									<AlertDialogCancel>Annuler</AlertDialogCancel>
-									<UpdateSupplierStatusButton
+									<DeleteSupplierButton
 										organizationId={supplier.organizationId}
 										id={supplier.id}
-										status={SupplierStatus.ACTIVE}
 									>
-										<AlertDialogAction>Confirmer</AlertDialogAction>
-									</UpdateSupplierStatusButton>
+										<AlertDialogAction>Supprimer</AlertDialogAction>
+									</DeleteSupplierButton>
 								</AlertDialogFooter>
 							</AlertDialogContent>
 						</AlertDialog>
