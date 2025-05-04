065f1667b53b27c44e7cae351e6edeb7f57748ff
Adrien490 <adrien.poirier49@gmail.com>
2025-05-04 16:30:47 +0200

    Update db.ts

--- shared/lib/db.ts
+++ shared/lib/db.ts
@@ -1,17 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { withOptimize } from "@prisma/extension-optimize";
+// import { withOptimize } from "@prisma/extension-optimize";
+
 const globalForPrisma = global as unknown as {
 	prisma: PrismaClient;
 };
 
 const prisma =
-	globalForPrisma.prisma ||
-	new PrismaClient().$extends(withAccelerate()).$extends(
-		withOptimize({
-			apiKey: process.env.OPTIMIZE_API_KEY!,
-		})
-	);
+	globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
