e8a28233ed7c07150c1d4ebf0782207925b0109b
Adrien490 <adrien.poirier49@gmail.com>
2025-05-09 12:38:22 +0200

    Update index.ts

--- domains/auth/features/sign-in-social/index.ts
+++ domains/auth/features/sign-in-social/index.ts
@@ -1,5 +1,4 @@
-export * from "./actions/signin-with-social-provider";
-export * from "./components/signin-with-social-provider-form";
+export * from "./components/sign-in-social-form";
 export * from "./hooks/use-sign-in-social";
 export * from "./schemas";
 export * from "./types";
