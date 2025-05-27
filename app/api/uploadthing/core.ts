3bb5e51ccd4b03e6f435410c902d22a6ca000f1c
Adrien490 <adrien.poirier49@gmail.com>
2025-05-27 11:42:06 +0200

    test

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -17,69 +17,6 @@ const getUser = async () => {
 
 // FileRouter pour vos fichiers uploadés
 export const ourFileRouter = {
-	// Define as many FileRoutes as you like, each with a unique routeSlug
-	groupImage: f({
-		image: {
-			/**
-			 * For full list of options and defaults, see the File Route API reference
-			 * @see https://docs.uploadthing.com/file-routes#route-config
-			 */
-			maxFileSize: "4MB",
-			maxFileCount: 1,
-			contentDisposition: "inline",
-		},
-	})
-		// Set permissions and file types for this FileRoute
-		.middleware(async () => {
-			const user = await getUser();
-
-			if (!user)
-				throw new UploadThingError(
-					"Vous devez être connecté pour télécharger une image"
-				);
-
-			// Whatever is returned here is accessible in onUploadComplete as `metadata`
-			return { userId: user.id };
-		})
-		.onUploadComplete(async ({ metadata, file }) => {
-			// This code RUNS ON YOUR SERVER after upload
-			console.log(
-				"Upload d'image de groupe complété pour l'utilisateur:",
-				metadata.userId
-			);
-			console.log("URL du fichier:", file.ufsUrl);
-
-			// !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-			return { url: file.ufsUrl };
-		}),
-
-	organizationLogo: f({
-		image: {
-			maxFileSize: "2MB",
-			maxFileCount: 1,
-			contentDisposition: "inline",
-		},
-	})
-		.middleware(async () => {
-			const user = await getUser();
-
-			if (!user)
-				throw new UploadThingError(
-					"Vous devez être connecté pour télécharger un logo"
-				);
-
-			return { userId: user.id };
-		})
-		.onUploadComplete(async ({ metadata, file }) => {
-			console.log(
-				"Upload de logo d'organisation complété pour l'utilisateur:",
-				metadata.userId
-			);
-			console.log("URL du fichier:", file.ufsUrl);
-
-			return { url: file.ufsUrl };
-		}),
-
 	companyLogo: f({
 		image: {
 			maxFileSize: "2MB",
